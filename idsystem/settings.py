832fc265fadf8f78174c17776aeffb8c3b9e7e88
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-02-06 14:51:08 +0530

    rest framework added

--- idsystem/settings.py
+++ idsystem/settings.py
@@ -42,9 +42,13 @@ INSTALLED_APPS = [
     'rest_framework'
 ]
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 
