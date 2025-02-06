1ed4d4408cf643b3076ff2a7e6fb1382d2fcc564
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-02-06 14:48:26 +0530

    create user model setup

--- Auth/models.py
+++ Auth/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import AbstractBaseUser , BaseUserManager, PermissionsMixin
+import random
+import string
 
-class CustomUser:
-    email =Charfield
+class CustomUserManager(BaseUserManager):
+    def create_user(self , email,name,passsword=None):
+        if not email:
+            raise ValueError("user must have an email")
+        while True :
+            user_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k =5))
+            if not self.model.objects.filter(user_id=user_id).exists():
+                break
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+            user_id=user_id
+        )
+        user.set_password(passsword)
+        user.save(using=self.db)
+        return user
+    
+
+
