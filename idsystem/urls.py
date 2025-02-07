0c0412274ca886405280c528b26472af389bf5d2
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-02-07 10:12:02 +0530

    urls has been updated

--- idsystem/urls.py
+++ idsystem/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from routing import router
+from .routing import router
 from django.urls import include, path
 
 urlpatterns = [
