6cb0dc4e7c8d1de5eef3facb1d4c872aa11e82a2
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-02-07 10:11:28 +0530

    views added

--- Auth/views.py
+++ Auth/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
