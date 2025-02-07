fb9c5df938d9ba808243f0af0fef7673e584fc19
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-02-07 10:52:33 +0530

    settings updated

--- idsystem/settings.py
+++ idsystem/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
     'Auth',
     'rest_framework'
 ]
@@ -45,6 +46,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
