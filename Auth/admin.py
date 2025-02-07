24d75e21db26d82bfac4b8c5665c6af7384e9ad2
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-02-07 15:06:33 +0530

    added admin panel

--- Auth/admin.py
+++ Auth/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CustomUser
 
+
+class UserModel(admin.ModelAdmin):
+    list_display=['user_id','email','name','is_paid']
 # Register your models here.
+admin.site.register(CustomUser,UserModel)
