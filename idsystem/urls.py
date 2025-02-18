7b1c492a5afa973b3f84d13a2d2ccd3bbede6e46
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-02-18 12:55:23 +0530

    added redoc to urls

--- idsystem/urls.py
+++ idsystem/urls.py
@@ -1,24 +1,15 @@
-"""idsystem URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from .routing import router
 from django.urls import include, path
+from .routing import router  # Your DRF API routes
+from .docs import schema_view  # Your OpenAPI schema
 
 urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),
     path('admin/', admin.site.urls),
-    path('api/',include(router.urls))
+    path('api/', include(router.urls)),  # Include your API endpoints
+
+    # Swagger UI:
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # Redoc UI:
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
