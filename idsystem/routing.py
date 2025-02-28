697855b0ad131597de38146c77a64c02c248de09
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-02-28 12:37:00 +0530

    routing updated

--- idsystem/routing.py
+++ idsystem/routing.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from Auth.views import CustomUserViewSet
+from Auth.views import CustomUserViewSet,generate_id_card
 
 router = DefaultRouter() 
 
-router.register(r'auth', CustomUserViewSet, basename='auth')
+router.register(r'auth', CustomUserViewSet, basename='auth'),
+router.register(r'id',generate_id_card,basename='id')
