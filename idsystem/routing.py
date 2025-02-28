471c1618b92c3a71eafd151fde7afe5922387d52
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-02-28 14:06:47 +0530

    fixed wrong url in routing

--- idsystem/routing.py
+++ idsystem/routing.py
@@ -3,5 +3,4 @@ from Auth.views import CustomUserViewSet,generate_id_card
 
 router = DefaultRouter() 
 
-router.register(r'auth', CustomUserViewSet, basename='auth'),
-router.register(r'id',generate_id_card,basename='id')
+router.register(r'auth', CustomUserViewSet, basename='auth')
