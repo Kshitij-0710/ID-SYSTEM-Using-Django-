292a2c5cd821b902a5e25599e29aeb5ff4935193
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-02-28 12:36:42 +0530

    added logic for generating an id card

--- Auth/views.py
+++ Auth/views.py
@@ -6,6 +6,11 @@ from django.contrib.auth import get_user_model, authenticate
 from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.authtoken.models import Token
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import get_template
+from weasyprint import HTML
+from .models import CustomUser
 
 User = get_user_model()
 
@@ -61,3 +66,17 @@ class CustomUserViewSet(viewsets.ModelViewSet):
             return Response({
                 'error': 'Invalid credentials'
             }, status=status.HTTP_400_BAD_REQUEST)
+
+def generate_id_card(request, user_id):
+    try:
+        user = CustomUser.objects.get(user_id=user_id)
+    except CustomUser.DoesNotExist:
+        return HttpResponse("User not found", status=404)
+    
+    template = get_template('id_card.html')
+    html_content = template.render({'user': user})
+    pdf_file = HTML(string=html_content).write_pdf()
+    
+    response = HttpResponse(pdf_file, content_type='application/pdf')
+    response['Content-Disposition'] = f'filename={user.user_id}_id_card.pdf'
+    return response
