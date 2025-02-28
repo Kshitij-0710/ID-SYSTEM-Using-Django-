ce9458cc7da8b1762d59953fd4098e0c32bcec26
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-02-28 14:07:30 +0530

    urls.py updated for index page

--- idsystem/urls.py
+++ idsystem/urls.py
@@ -1,15 +1,17 @@
+# idsystem/idsystem/urls.py
 from django.contrib import admin
 from django.urls import include, path
-from .routing import router  # Your DRF API routes
-from .docs import schema_view  # Your OpenAPI schema
+from .routing import router
+from Auth.views import generate_id_card  
+from .docs import schema_view  
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),  # Include your API endpoints
-
-    # Swagger UI:
+    path('api/', include(router.urls)),  
+    path('id-card/<str:user_id>/', generate_id_card, name='generate_id_card'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # Redoc UI:
+    path('auth-page/', TemplateView.as_view(template_name='index.html'), name='auth_page'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
